b245118fc5df866da2fbdb41469e896f62145d16
dltbwoddl <60048456+dltbwoddl@users.noreply.github.com>
2020-10-04 13:34:37 +0900

    Update Home.vue

--- 방구석월드컵/Frontend학습(vuejs)/test1/src/views/Home.vue
+++ 방구석월드컵/Frontend학습(vuejs)/test1/src/views/Home.vue
@@ -2,28 +2,59 @@
   <div>
     <h1>this is Home page</h1>
     <p>{{names}}</p>
-    <balmostory>
+    <button v-on:click="dateupdate()">click</button>
+    <!-- <balmostory>
      <template v-slot:first="Home" >
         <p>hello {{Home.homes}}</p>
      </template>
      <template v-slot:default>
        <p>hi</p>
      </template>
-    </balmostory>
+    </balmostory> -->
   </div>
 </template>
 
 <script>
-import balmostory from '@/components/balmostory.vue'
+// import balmostory from '@/components/balmostory.vue'
 // 컴포넌트 불러오기
 export default{
   // 컴포넌트 등록하기.
-  components:{
-    balmostory
-  },
+  // components:{
+  //   balmostory
+  // },
   data(){
    return{
      names:"balmostory"
+    }
+  },
+  beforeCreate(){
+    console.log("beforeCreate",this.names)
+  },
+  created(){
+    console.log("created",this.names)
+  },
+  beforeMount(){
+    alert("beforeMount")
+  }
+  ,
+  mounted(){
+    alert("mounted")
+  },
+  beforeUpdate(){
+    alert("beforeUpdate")
+  },
+  updated(){
+    alert("updated")
+  },
+  beforeDestroy(){
+    alert("beforeDestroy")
+  },
+  destroyed(){
+    alert("destroyed")
+  },
+  methods:{
+    dateupdate(){
+      this.names="hihihi";
     }
   }
 }
