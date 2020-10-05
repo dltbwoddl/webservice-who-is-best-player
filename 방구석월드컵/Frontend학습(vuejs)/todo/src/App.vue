7e74faefa146f89481d8cb9c4b76a7bb7a1f580f
dltbwoddl <60048456+dltbwoddl@users.noreply.github.com>
2020-10-05 14:13:15 +0900

    6
    6

---
+++ 방구석월드컵/Frontend학습(vuejs)/todo/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app" class="container">
+    <h1 class="text-center">Todo App</h1>
+    <input 
+    v-model="emptytext"
+    type="text" 
+    class="w-100 p-2" 
+    placeholder="Type todo"
+    @keyup.enter="plustodo">
+    <hr>
+    <Todo 
+    v-for="todo in todos" 
+    :key="todo.id" 
+    :todo="todo" />
+  </div>
+</template>
+
+<script>
+import Todo from "@/components/Todo.vue"
+
+export default {
+components:{
+  Todo
+},
+data(){
+  return{
+    todos:[
+      {id:1, text:"buy a car", checked:false}
+    , {id:2, text:"play game", checked:false}
+    ],
+    emptytext:""
+  }
+},
+methods:{
+  plustodo(e){
+    console.log(e.target.value)
+    console.log(this.todos[this.todos.length-1].text)
+    this.todos.push({
+      id:this.todos[this.todos.length-1]+1,
+      text:e.target.value,
+      checked:false
+      })
+      this.emptytext="";
+    }
+  }
+}
+</script>
---
+++ 방구석월드컵/Frontend학습(vuejs)/todo/src/components/Todo.vue
@@ -0,0 +1,18 @@
+
+<template>
+    <div>
+      <input type="checkbox" :checked="todo.checked">
+      <span class="ml-3">{{todo.text}}</span>
+    </div>
+</template>
+
+<script>
+export default{
+    props:{
+        todo:{
+            type:Object,
+            required:false
+        }
+    }
+}
+</script>
